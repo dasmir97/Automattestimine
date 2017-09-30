83dd1e51d1b0dccef71f1e312ce8a933e949fee2
dasmir <dasmir@ttu.ee>
2017-09-30 17:37:37 +0300

    Junit init

---
+++ utest/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public double hetkeTemperatuur() {
+        return 0;
+    }
+
+    public double korgeimTemperatuur() {
+        return 0;
+    }
+
+    public double madalaimTemperatuur() {
+        return 0;
+    }
+
+    public double[] koordinaadid() {
+        return new double[2];
+    }
+}
